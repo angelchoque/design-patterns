62eabafe84706a334fe24bd90f07d795d00730bd
angel choque <miguelangelpacompia@gmail.com>
2022-12-08 23:50:13 -0500

    ts: herencia

--- src/intro/ts/intro.ts
+++ src/intro/ts/intro.ts
@@ -12,3 +12,20 @@ class Drink {
 
 const awa = new Drink("awa")
 console.log(awa.info())
+
+// Herencia
+class Beer extends Drink {
+  private alcohol: number
+  constructor(name: string, alcohol: number) {
+    super(name)
+    this.alcohol = alcohol
+  }
+
+  info() {
+    return super.info() + " " + this.alcohol
+  }
+
+}
+
+const beer = new Beer("unknown", 4.8)
+console.log("🚀 ~ ", beer.info())
