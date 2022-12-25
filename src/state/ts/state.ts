7054c3ada51f9aea1d18d7339e99eddff5e52757
angel choque <miguelangelpacompia@gmail.com>
2022-12-24 21:55:43 -0500

    state ts

---
+++ src/state/ts/state.ts
@@ -0,0 +1,126 @@
+interface State {
+
+  next(ticket: Ticket): number | null
+  add(ticket: Ticket, quantity: number): void
+
+}
+
+class Ticket {
+
+  private state: State
+  quantity: number
+  readonly limit: number
+  private number: number // <- current number
+
+  constructor(limit: number) {
+
+    this.limit = limit
+    this.quantity = 0
+    this.number = 0
+    this.state = new EmptyState() // <- initial state
+  }
+
+  get getNumber(): number {
+    return this.number++
+  }
+
+  set setState(state: State) {
+    this.state = state
+  }
+
+  get getState() {
+    return this.state
+  }
+
+  next(): number | null{
+    return this.state.next(this)
+  }
+
+  add(quantity: number): void {
+    this.state.add(this, quantity)
+  }
+
+}
+
+class EmptyState implements State {
+
+  next(ticket: Ticket): number | null {
+    return null
+  }
+
+  add(ticket: Ticket, quantity: number): void {
+    if (quantity < ticket.limit) {
+      ticket.quantity = quantity
+      ticket.setState = new WithDataState()
+    } else if (quantity === ticket.limit){
+      ticket.quantity = quantity
+      ticket.setState = new FullState()
+    }
+  }
+
+}
+
+class WithDataState implements State {
+
+  next(ticket: Ticket): number | null {
+    ticket.quantity--
+    if (ticket.quantity <= 0) {
+      ticket.setState = new EmptyState()
+    }
+    return ticket.getNumber
+  }
+
+  add(ticket: Ticket, quantity: number): void {
+    if ((ticket.quantity + quantity) < ticket.limit) {
+      ticket.quantity += quantity
+    } else if ((ticket.quantity + quantity)=== ticket.limit){
+      ticket.quantity += quantity
+      ticket.setState = new FullState()
+    }
+  }
+
+}
+
+class FullState implements State {
+
+  next(ticket: Ticket): number | null {
+    ticket.quantity--
+    if (ticket.quantity <= 0) {
+      ticket.setState = new EmptyState()
+    } else {
+      ticket.setState = new WithDataState()
+    }
+    return ticket.getNumber
+  }
+
+  add(ticket: Ticket, quantity: number): void {
+    console.log('====================================')
+    console.log("Ticket Lleno")
+    console.log('====================================')
+  }
+
+}
+
+// x
+const ticket = new Ticket(5)
+console.log(ticket.getState)
+console.log(ticket.next())
+ticket.add(6)
+console.log(ticket.getState)
+console.log(ticket.next())
+ticket.add(4)
+console.log(ticket.getState)
+console.log(ticket.next())
+console.log(ticket.next())
+ticket.add(3)
+console.log(ticket.getState)
+ticket.add(1)
+console.log(ticket.next())
+console.log(ticket.getState)
+console.log(ticket.next())
+console.log(ticket.next())
+console.log(ticket.next())
+console.log(ticket.next())
+console.log(ticket.getState)
+console.log(ticket.next())
+
